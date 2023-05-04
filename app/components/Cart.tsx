9c6204b8495ea811aacec2c5dc5ff4f4fa29f0dd
Winterson <u19cse1028@cit.ac.in>
2023-05-05 00:09:45 +0530

    Cart Functionality

--- app/components/Cart.tsx
+++ app/components/Cart.tsx
@@ -1,12 +1,30 @@
 'use client'
 import Image from "next/image"
 import { useDataStore } from "@/store"
+import formatPrice from "@/util/PriceFormat"
 
 export default function Cart(){
 	const cartStore = useDataStore()
 	return (
-		<div>
-			<h1>Cart</h1>
+		<div onClick={() => cartStore.toggleCart()} className="fixed w-full h-screen left-0 top-0 bg-black/25">
+				<section onClick={(e)=>e.stopPropagation()} className="bg-white absolute right-0 top-0 h-screen w-1/4 p-5 overflow-y-scroll text-gray-700">
+				<main className="flex flex-col">
+					<h1>Your Shopping Cart.</h1>	
+					{
+						cartStore.cart.map((item) => (						 
+							<div className="flex py-4 gap-4">
+								<Image src={item.image} alt={item.name} width={120} height={120} />
+								<div className="flex flex-col">
+									<h2 className="text-sm">{item.name}</h2>
+									<h2 className="text-sm">Quantity: {item.quantity}</h2>
+									<p className="text-sm">{formatPrice(item.price)}</p>
+								</div>
+							</div>
+						)
+					)}
+					<button className="btn btn-base w-full mt-10 self-center items-center bg-teal-600 border-0 text-lg">Checkout</button>
+				</main>
+				</section>	
 		</div>
 	)
 }
